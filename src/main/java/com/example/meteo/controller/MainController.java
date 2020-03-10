eca6494c39bf67ba72a5d0df01b64f09b6f68488
Anatolii Marandiuc <shamaneeel@mail.ru>
2020-03-10 23:54:25 +0300

    client cabinet

--- src/main/java/com/example/meteo/controller/MainController.java
+++ src/main/java/com/example/meteo/controller/MainController.java
@@ -1,7 +1,10 @@
 package com.example.meteo.controller;
 
+import com.example.meteo.model.User;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -19,5 +22,15 @@ public class MainController {
     @GetMapping("weather")
     public String getDefaultWeather(){
         return "defaultWeather";
+    }
+
+    @GetMapping("lc")
+    public ModelAndView lc(Authentication auth) {
+        ModelAndView model = new ModelAndView("lc");
+        User user = (User) auth.getPrincipal();
+        model.addObject("email", user.getEmail());
+
+
+        return model;
     }
 }
